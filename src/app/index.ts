f93d03cc4e9f325acbbe7791ddf40826156e0ad7
nikitsch <nikitsch5@gmail.com>
2024-04-15 21:58:09 +0300

    feat: translation

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,5 @@
-import '../shared/styles/style.css'
 import '../shared/styles/reset.css'
+import '../shared/styles/style.css'
 import batteryIcon from '../shared/assets/svg/battery.svg'
 import bluetoothIcon from '../shared/assets/svg/bluetooth.svg'
 import crossIcon from '../shared/assets/svg/cross.svg'
@@ -52,43 +52,43 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <img src="${crossIcon}" alt="cross">
       </a>
 
-      <h1 class="main-title">Get Unlimited <br>Access</h1>
+      <h1 class="main-title" data-i18n="Get Unlimited <br>Access">Get Unlimited <br>Access</h1>
       
       <section class="poster-gallery">
         <article class="poster">
           <img src="${posterFirst1xWebp}" srcset="${posterFirst2xWebp} 2x, ${posterFirst3xWebp} 3x" alt="poster-1">
-          <h3 class="poster-title">Unlimited Art <br>Creation</h3>
+          <h3 class="poster-title" data-i18n="Unlimited Art <br>Creation">Unlimited Art <br>Creation</h3>
           </article>
           <article class="poster">
           <img src="${posterSecond1xWebp}" srcset="${posterSecond2xWebp} 2x, ${posterSecond3xWebp} 3x" alt="poster-2">
-          <h3 class="poster-title">Exclusive Styles</h3>
+          <h3 class="poster-title" data-i18n="Exclusive <br>Styles">Exclusive Styles</h3>
         </article>
         <article class="poster">
           <img src="${posterThird1xWebp}" srcset="${posterThird2xWebp} 2x, ${posterThird3xWebp} 3x" alt="poster-3">
-          <h3 class="poster-title">Magic Avatars With 20% Off</h3>
+          <h3 class="poster-title" data-i18n="Magic Avatars <br>With 20% Off">Magic Avatars With 20% Off</h3>
         </article>
       </section>
 
       <section class="interaction">
         <button class="first-option-btn btn active option-btn" id="option-1" data-href="https://apple.com/">
           <div class="left-btn-align">
-            <p class="btn-title">YEARLY ACCESS</p>
-            <p class="btn-subtitle">Just $39.99 per year</p>
+            <p class="btn-title" data-i18n="YEARLY ACCESS">YEARLY ACCESS</p>
+            <p class="btn-subtitle" data-i18n="Just {{price}} per year">Just $39.99 per year</p>
           </div>
           <div class="right-btn-align">
-            <p class="btn-subtitle">$0.48 <br/>per week</p>
+            <p class="btn-subtitle" data-i18n="{{price}} <br>per week">$0.48 <br/>per week</p>
           </div>
-          <span class="btn-note">BEST OFFER</span>
+          <span class="btn-note" data-i18n="BEST OFFER">BEST OFFER</span>
         </button>
         <button class="second-option-btn btn option-btn" id="option-2" data-href="https://google.com/">
           <div class="left-btn-align">
-            <p class="btn-title">WEEKLY ACCESS</p>
+            <p class="btn-title" data-i18n="WEEKLY ACCESS">WEEKLY ACCESS</p>
           </div>
           <div class="right-btn-align">
-            <p class="btn-subtitle">$6.99 <br/>per week</p>
+            <p class="btn-subtitle" data-i18n="{{price}} <br>per week">$6.99 <br/>per week</p>
           </div>
         </button>
-        <button class="submit-btn btn" id="submit">Continue</button>
+        <button class="submit-btn btn" id="submit" data-i18n="Continue">Continue</button>
       </section>
   </main>
 
@@ -96,13 +96,13 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <nav class="navigation">
       <ul class="nav-list">
         <li class="nav-item">
-          <a href="#" class="nav-link">Terms of Use</a>
+          <a href="#" class="nav-link" data-i18n="Terms of Use">Terms of Use</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link">Privacy Policy</a>
+          <a href="#" class="nav-link" data-i18n="Privacy Policy">Privacy Policy</a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link">Restore</a>
+          <a href="#" class="nav-link" data-i18n="Restore">Restore</a>
         </li>
       </ul>
     </nav>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,53 @@
+const languages = ['de', 'en', 'es', 'fr', 'ja', 'pt'] as const;
+type LanguagesType = (typeof languages)[number];
+const userParam = navigator.language.split('-')[0];
+const langParam = new URLSearchParams(window.location.search).get('lang');
+
+const loadLanguage = async (lang: LanguagesType) => {
+  try {
+    const module = await import(`./shared/languages/${lang}.json`);
+    return module.default;
+  } catch (error) {
+    console.error('Failed to load language file:', error);
+    return null;
+  }
+};
+
+async function applyTranslations(language: LanguagesType) {
+  const translation = await loadLanguage(language);
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+    const key = element.getAttribute('data-i18n');
+    if (key && translation[key as keyof typeof translation]) {
+      element.innerHTML = translation[key as keyof typeof translation];
+    }
+  });
+}
+
+function reloadLanguage () {
+  if (langParam?.length === 2 && languages.includes(langParam as LanguagesType)) {
+    applyTranslations(langParam as LanguagesType);
+  } else if (languages.includes(userParam as LanguagesType)) {
+    applyTranslations(userParam as LanguagesType);
+  } else {
+    applyTranslations('en');
+  }
+}
+
+reloadLanguage();
+
+//!target on interaction
 document.getElementById("option-1")!.addEventListener("click", function() {
   const option1 = document.getElementById("option-1");
   const option2 = document.getElementById("option-2");
   
   if (option1 && option2) {
-      option2.classList.remove("active");
-      option1.classList.add("active");
+    option2.classList.remove("active");
+    option1.classList.add("active");
 
-      const href = option1.getAttribute("data-href");
-      if (href) {
-          localStorage.setItem("selectedHref", href);
-      }
+    const href = option1.getAttribute("data-href");
+    if (href) {
+      localStorage.setItem("selectedHref", href);
+    }
   }
 });
 
@@ -18,19 +56,19 @@ document.getElementById("option-2")!.addEventListener("click", function() {
   const option2 = document.getElementById("option-2");
 
   if (option1 && option2) {
-      option1.classList.remove("active");
-      option2.classList.add("active");
+    option1.classList.remove("active");
+    option2.classList.add("active");
 
-      const href = option2.getAttribute("data-href");
-      if (href) {
-          localStorage.setItem("selectedHref", href);
-      }
+    const href = option2.getAttribute("data-href");
+    if (href) {
+      localStorage.setItem("selectedHref", href);
+    }
   }
 });
 
 document.getElementById("submit")!.addEventListener("click", function() {
   const href = localStorage.getItem("selectedHref");
   if (href) {
-      window.open(href, '_blank');
+    window.open(href, '_blank');
   }
 });
